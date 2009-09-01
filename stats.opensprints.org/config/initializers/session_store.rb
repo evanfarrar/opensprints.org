45e5b535da8031d68eeef606bf2a02e78d0f39c6
Evan Farrar <evanfarrar@gmail.com>
2009-09-01 12:31:17 -0500

    Added login/registration/sessions with clearance

--- stats.opensprints.org/config/initializers/session_store.rb
+++ stats.opensprints.org/config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key    => "_CHANGEME_session",
-  :secret => "CHANGESESSION"
+  :key    => "_opensprints_stats_session",
+  :secret => "7ec22d07285bdf0881b89b7d41d5cfdf"
 }
 
 # Use the database for sessions instead of the cookie-based default,
