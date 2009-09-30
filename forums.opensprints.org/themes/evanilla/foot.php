fedb3f1d9b7435df3d8cebc63efed4e1201841d2
Evan Farrar <evanfarrar@gmail.com>
2009-09-29 22:02:57 -0500

    Skins the forums with the new look and feel.

---
+++ forums.opensprints.org/themes/evanilla/foot.php
@@ -0,0 +1,36 @@
+<?php
+// Note: This file is included from the library/Vanilla/Vanilla.Control.Foot.php class.
+
+	echo '</div>
+    </div>
+	<a id="pgbottom" name="pgbottom">&nbsp;</a>
+	</div>
+</div>
+<div id="footer">
+  <span class="button">
+    <a href="http://twitter.com/opensprints">Follow us on twitter</a>
+  </span>
+  <span class="button">
+    <a href="http://twitter.com/opensprints">Add us on facebook</a>
+  </span>
+  <span>
+    &copy; 2009 OpenSprints L.L.C. Some rights reserved.
+    <a href="http://creativecommons.org/licenses/by/3.0/us/" rel="license">
+      <img alt="Creative Commons License" src="http://i.creativecommons.org/l/by/3.0/us/80x15.png" style="border-width:0" />
+    </a>
+  </span>
+</div>';
+
+$AllowDebugInfo = 0;
+if ($this->Context->Session->User) {
+	if ($this->Context->Session->User->Permission('PERMISSION_ALLOW_DEBUG_INFO')) $AllowDebugInfo = 1;
+}
+if ($this->Context->Mode == MODE_DEBUG && $AllowDebugInfo) {
+	echo '<div class="DebugBar" id="DebugBar">
+	<b>Debug Options</b> | Resize: <a href="javascript:window.resizeTo(800,600);">800x600</a>, <a href="javascript:window.resizeTo(1024, 768);">1024x768</a> | <a href="'
+	."javascript:HideElement('DebugBar');"
+	.'">Hide This</a>';
+	echo $this->Context->SqlCollector->GetMessages();
+	echo '</div>';
+}
+?>
---
+++ forums.opensprints.org/themes/evanilla/head.php
@@ -0,0 +1,72 @@
+<?php
+// Note: This file is included from the library/Framework/Framework.Control.Head.php class.
+
+$HeadString = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="'.$this->Context->GetDefinition('XMLLang').'">
+	<head>
+		<title>'.$this->Context->Configuration['APPLICATION_TITLE'].' - '.$this->Context->PageTitle.'</title>
+		<link rel="shortcut icon" href="'.$this->Context->StyleUrl.'favicon.ico" />';
+
+		while (list($Name, $Content) = each($this->Meta)) {
+			$HeadString .= '
+			<meta name="'.$Name.'" content="'.$Content.'" />';
+		}
+
+		if (is_array($this->StyleSheets)) {
+			while (list($Key, $StyleSheet) = each($this->StyleSheets)) {
+				$HeadString .= '
+				<link rel="stylesheet" type="text/css" href="'.$StyleSheet['Sheet'].'"'.($StyleSheet['Media'] == ''?'':' media="'.$StyleSheet['Media'].'"').' />';
+			}
+		}
+    $HeadString .= '<link rel="stylesheet" type="text/css" href="http://opensprints.org/stylesheets/screen.css" />';
+    $HeadString .= '<link rel="stylesheet" type="text/css" href="http://opensprints.org/stylesheets/application.css" />';
+		if (is_array($this->Scripts)) {
+			$ScriptCount = count($this->Scripts);
+			$i = 0;
+			for ($i = 0; $i < $ScriptCount; $i++) {
+				$HeadString .= '
+				<script type="text/javascript" src="'.$this->Scripts[$i].'"></script>';
+			}
+		}
+
+		if (is_array($this->Strings)) {
+			$StringCount = count($this->Strings);
+			$i = 0;
+			for ($i = 0; $i < $StringCount; $i++) {
+				$HeadString .= $this->Strings[$i];
+			}
+		}
+$BodyId = "";
+if ($this->BodyId != "") $BodyId = ' id="'.$this->BodyId.'"';
+echo $HeadString . '</head>
+	<body'.$BodyId.' '.$this->Context->BodyAttributes.'>
+	<div id="SiteContainer" class="container">
+      <div class="span-5 append-1 menu">
+        <div class="header"></div>
+        <ul>
+          <li>
+            <a href="http://opensprints.org/about.html">ABOUT</a>
+          </li>
+          <li>
+            <a href="http://opensprints.org/news">NEWS</a>
+          </li>
+          <li>
+            <a href="http://wiki.opensprints.org">DOCUMENTATION</a>
+          </li>
+          <li>
+            <span>SUPPORT &amp; DISCUSSION</span>
+          </li>
+          <li>
+            <a href="http://opensprints.org//downloads.html">DOWNLOADS</a>
+          </li>
+          <li>
+            <a href="http://opensprints.org/contact.html">PRODUCTS</a>
+          </li>
+          <li class="louder">
+            <a href="http://opensprints.org/contact.html">CONTACT</a>
+          </li>
+        </ul>
+      </div>
+      <div class="intro span-17">
+        <div class="header_clear"></div>';
+?>
