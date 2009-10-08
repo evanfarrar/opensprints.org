8d3485037fd10ed51488f106c485b855fdebf40b
Evan Farrar <evanfarrar@gmail.com>
2009-10-07 20:23:00 -0500

    Larger images.

--- store.opensprints.org/app/models/image.rb
+++ store.opensprints.org/app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < Asset
   has_attached_file :attachment, 
-                    :styles => { :mini => '48x48>', :small => '100x100>', :product => '240x240>', :large => '600x600>' }, 
+                    :styles => { :mini => '100x100>', :small => '240x240>', :product => '400x400>', :large => '600x600>' }, 
                     :default_style => :product,
                     :url => "/assets/products/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
