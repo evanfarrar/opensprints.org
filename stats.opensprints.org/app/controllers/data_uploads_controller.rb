9e0aee8f3da8ae3f646c9a9082110e38249885ef
Evan Farrar <evanfarrar@gmail.com>
2009-10-12 22:14:21 -0500

    Now users can upload a database file.

--- stats.opensprints.org/app/controllers/data_uploads_controller.rb
+++ stats.opensprints.org/app/controllers/data_uploads_controller.rb
@@ -1,2 +1,15 @@
 class DataUploadsController < ApplicationController
+  def new
+    @data_upload = DataUpload.new
+  end
+
+  def create
+    params[:data_upload].merge!({:user_id => current_user.id})
+    @data_upload = DataUpload.create(params[:data_upload])
+    redirect_to :action => :index
+  end
+
+  def index
+    @data_uploads = DataUpload.find(:all,:conditions => {:user_id => current_user.id})
+  end
 end
--- stats.opensprints.org/app/controllers/group_controller.rb
+++ stats.opensprints.org/app/controllers/group_controller.rb
@@ -1,6 +1,5 @@
 class GroupController < ApplicationController
   def show
     @user = User.find_by_group_name_for_url(params[:group_name])
-    render :text => params[:group_name]
   end
 end
