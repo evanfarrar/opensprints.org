815b425faa57a4c0fe54d5acecb5d452eea377e9
Evan Farrar <evanfarrar@gmail.com>
2009-10-16 20:33:15 -0500

    I was really loving suspenders...until I wasted a day trying to set up email only to realize an initializer was pointing me to smtp.thoughtbout.com...

--- stats.opensprints.org/config/initializers/action_mailer_configs.rb
+++ stats.opensprints.org/config/initializers/action_mailer_configs.rb
@@ -1,6 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-    :address => "smtp.thoughtbot.com",
-    :port    => 25,
-    :domain  => "thoughtbot.com"
+  :address => 'mail.opensprints.com',
+  :port => 587,
+  :domain => 'opensprints.com',
+  :authentication => :login,
+  :user_name => 'system@opensprints.com',
+  :password => 'password'
 }
 
